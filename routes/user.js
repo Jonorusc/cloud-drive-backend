c281cdabeb77514aacad2115a6aa8f198bcbf3f0
João Lucas <joaolucascp16@gmail.com>
2022-06-21 12:51:30 -0300

    user.js: send another verification email

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const { authUser } = require('../middlewares/auth'),
-    { register, activateAccount, login } = require('../controllers/user'),
+    { register, activateAccount, login, sendVerification } = require('../controllers/user'),
     express = require('express'),
     router = express.Router()
 
 router.post('/register', register)
 router.post('/activate', authUser, activateAccount)
 router.post('/login', login)
+router.post('/sendverification', authUser, sendVerification)
 
 module.exports = router
