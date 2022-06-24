272a710ca342c59b020be19065a2a30b523ee86b
João Lucas <joaolucascp16@gmail.com>
2022-06-23 22:36:12 -0300

    user.js: new routes

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,14 @@
 const { authUser } = require('../middlewares/auth'),
-    { register, activateAccount, login, sendVerification } = require('../controllers/user'),
+    {
+        register,
+        activateAccount,
+        login,
+        sendVerification,
+        findUser,
+        sendResetCode,
+        validateResetCode,
+        changePassword,
+    } = require('../controllers/user'),
     express = require('express'),
     router = express.Router()
 
@@ -7,5 +16,10 @@ router.post('/register', register)
 router.post('/activate', authUser, activateAccount)
 router.post('/login', login)
 router.post('/sendverification', authUser, sendVerification)
+router.post('/finduser', findUser)
+router.post('/sendresetcode', sendResetCode)
+router.post('/validateresetcode', validateResetCode)
+router.post('/changepassword', changePassword)
+
 
 module.exports = router
