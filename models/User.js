2f4bcfa1ec7d1be536155c5827f5f0d44440d077
João Lucas <joaolucascp16@gmail.com>
2022-06-25 17:19:42 -0300

    User.js: drive update

--- models/User.js
+++ models/User.js
@@ -1,56 +1,62 @@
 const mongoose = require('mongoose'),
     { ObjectId } = mongoose.Schema,
-    userSchema = mongoose.Schema({
-        first_name: {
-            type: String,
-            required: [true, 'First name is required'],
-            text: true,
-        },
-        last_name: {
-            type: String,
-            required: [true, 'First name is required'],
-            text: true,
-        },
-        username: {
-            type: String,
-            required: [true, 'Username is required'],
-            trim: true,
-            text: true,
-            unique: true,
-        },
-        email: {
-            type: String,
-            required: [true, 'Email is required'],
-            trim: true,
-        },
-        password: {
-            type: String,
-            required: [true, 'Password is required'],
-            trim: true,
-        },
-        picture: {
-            type: String,
-            trim: true,
-            default: 'https://cdn-icons-png.flaticon.com/512/21/21104.png'
-        },
-        verified: {
-            type: Boolean,
-            default: false,
-        },
-        drive: [{
-            folders: {
-                type: ObjectId,
-                id: null,
-                ref: 'files',
-                excluded: false,
+    userSchema = mongoose.Schema(
+        {
+            first_name: {
+                type: String,
+                required: [true, 'First name is required'],
+                text: true,
             },
-            files: {
-                type: ObjectId,
-                folder_id: null,
-                ref: 'files',
-                excluded: false,
-            }
-        }]
-    }, {timestamps: true})
+            last_name: {
+                type: String,
+                required: [true, 'First name is required'],
+                text: true,
+            },
+            username: {
+                type: String,
+                required: [true, 'Username is required'],
+                trim: true,
+                text: true,
+                unique: true,
+            },
+            email: {
+                type: String,
+                required: [true, 'Email is required'],
+                trim: true,
+            },
+            password: {
+                type: String,
+                required: [true, 'Password is required'],
+                trim: true,
+            },
+            picture: {
+                type: String,
+                trim: true,
+                default: 'https://cdn-icons-png.flaticon.com/512/21/21104.png',
+            },
+            verified: {
+                type: Boolean,
+                default: false,
+            },
+            drive: [
+                {
+                    folders: {
+                        type: String,
+                        id: null,
+                        ref: 'folder',
+                        excluded: false,
+                        files: [
+                            {
+                                type: String,
+                                ref: 'file',
+                                excluded: false,
+                            },
+                        ],
+                    },
+                },
+            ],
+        },
+        { timestamps: true }
+    )
 
-    module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User', userSchema)
